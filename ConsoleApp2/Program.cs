2aaadc1bce28ed1644e68c8e7d9ad9e1e7347fd8
Grubla <filip.aburg@gmail.com>
2021-04-03 02:04:16 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(){
+            int toAdd = (Console.ReadLine().Length - 2) * 2;
+            List<string> temp = new List<string>();
+            temp.Add("h");
+            for (int i = 0; i < toAdd; i++)
+            {
+                temp.Add("e");
+            }
+            temp.Add("y");
+            Console.WriteLine(string.Join("", temp));
+        }
+    }
+}
